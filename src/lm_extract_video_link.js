28b83d057e0ac57eda11cf20804e234ee6d30637
Ivaylo Valkov <ivaylo@e-valkov.org>
2011-12-01 11:51:15 +0000

    Remove console.log statemens left from debugging.

--- src/lm_extract_video_link.js
+++ src/lm_extract_video_link.js
@@ -122,14 +122,12 @@ LinternaMagica.prototype.extract_link = function()
 	//     // 	"mediashare\\\.bg|ted\\\.com",
 	//     "i");
 
-	console.log("Bui hu hu "+data);
 
 	// Some links used with flowplayer are relative
 	if (data.match(/.*flowplayer.*/))
 	{
 	    link = this.fix_flowplayer_links(link);
 	}
-	console.log("wee! But now what? ");
 
 	this.log("LinternaMagica.extract_link:\n"+
 		 " Extracted link: "+link,1);
