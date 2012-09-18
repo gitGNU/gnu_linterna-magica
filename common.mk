efe58574d4965c4af573793a9cc2516802d63496
Ivaylo Valkov <ivaylo@e-valkov.org>
2012-09-18 06:30:29 +0000

    Preparing for bug-fix release 0.0.12-2

--- common.mk
+++ common.mk
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Linterna Mágica.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = 0.0.12-1
+VERSION = 0.0.12-2
 PACKAGE = linternamagica
 GETTEXT_PACKAGE = linterna_magica
 
