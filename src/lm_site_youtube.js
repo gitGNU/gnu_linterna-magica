ab63a6dca66ed33d695f3bbf09ccc87871b0371e
Ivaylo Valkov <ivaylo@e-valkov.org>
2012-12-08 13:22:22 +0000

    Merging r340 from trunk to branch 0.0.12. Better fix for YouTube than r338 & r339 for bugs #37881.

--- src/lm_site_youtube.js
+++ src/lm_site_youtube.js
@@ -379,10 +379,12 @@ function()
 	embed_id= embed_id[embed_id.length-1];
     }
 
+    var p = this.get_first_element_by_class("flash-player");
+    p = p ? p : this.get_first_element_by_class("html5-player");
+
+
     if (!width || !height)
     {
-	var p = this.get_first_element_by_class("player-root");
-
 	height = p ? p.clientHeight : null;
 	width  = p ? p.clientWidth : null;
     }
@@ -434,7 +436,7 @@ function()
 	linterna_magica_id =
 	    this.mark_flash_object("extracted-from-script");
 
-	object_data.parent =  this.get_first_element_by_class("player-root");
+	object_data.parent =  p;
     }
 
     object_data.linterna_magica_id = linterna_magica_id;
