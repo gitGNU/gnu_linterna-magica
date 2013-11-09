eb1a571c8c060c57a6dc804492a779a965df7327
Ivaylo Valkov <ivaylo@e-valkov.org>
2013-11-09 19:10:45 +0200

    Fixed support for videolectures.net. See bugs #38400. https://savannah.nongnu.org/bugs/?38400
    The new code replaces the website HTML5 player and slides. The website
    HTML5 player does not work in Firefox.

--- src/lm_site_videolecturesnet.js
+++ src/lm_site_videolecturesnet.js
@@ -30,44 +30,205 @@ LinternaMagica.prototype.sites["videolectures.net"] = new Object();
 // Reference
 LinternaMagica.prototype.sites["www.videolectures.net"] = "videolectures.net";
 
-LinternaMagica.prototype.sites["videolectures.net"].skip_link_extraction =
-function()
-{
-    var see_also = document.getElementById("vl_seealso");
-
-    if (!see_also)
-    {
-	return null;
-    }
-
-    var ps = see_also.getElementsByTagName("p");
-
-    if (!ps)
-    {
-	return null;
-    }
-
-    var p = ps[0];
-    var link = p.getElementsByTagName("a");
-
-    if (!link)
-    {
-	return null;
-    }
-
-    link = link[0];
-
-    var extracted_data = new Object();
-    extracted_data.link = link.getAttribute("href");
-    // Breaks the web controls. Works fine in Totem without this.
-    // extracted_data.mime = "video/x-ms-wmv";
-
-    return extracted_data;
-
-}
 
 LinternaMagica.prototype.sites["videolectures.net"].do_not_force_iframe_detection =
 function()
 {
     return false;
+}
+
+LinternaMagica.prototype.sites["videolectures.net"].skip_script_processing = function()
+{
+    return false;
+}
+
+LinternaMagica.prototype.sites["videolectures.net"].skip_link_extraction = function()
+{
+    return false;
+}
+
+LinternaMagica.prototype.sites["videolectures.net"].
+    skip_video_id_extraction = function()
+{
+    return null;
+}
+
+
+LinternaMagica.prototype.sites["videolectures.net"].flash_plugin_installed = function()
+{
+    return this.sites["videolectures.net"].
+        no_flash_plugin_installed.apply(this,[arguments]);
+}
+
+LinternaMagica.prototype.sites["videolectures.net"].no_flash_plugin_installed = function()
+{
+    var el = document.getElementById('video_player');
+
+    if (!el)
+    {
+	return null;
+    }
+
+
+    var video_id = window.location.href.replace('#','')+"video/1/smil.xml";
+    var width = null;
+    var height = null;
+
+    var left_box = document.querySelectorAll('.lec_lbox');
+
+    if (left_box && left_box[0]) {
+	var t = left_box[0];
+	width = t.clientWidth || t.offsetWidth ||
+	    t.parentNode.clientWidth || t.parentNode.offsetWidth;
+
+	height = 320;
+    }
+
+    if (!width || !height)
+    {
+	width = 502;
+	height = 400;
+    }
+
+    if (!(width && height))
+    {
+	return null;
+    }
+
+    var cleanup = document.getElementById('video_border');
+
+    if (cleanup)
+    {
+	cleanup.parentNode.removeChild(cleanup);
+    }
+
+    object_data = new Object();
+
+    object_data.linterna_magica_id = 0;
+    object_data.width = width;
+    object_data.height = height;
+    object_data.video_id = video_id;
+    object_data.parent = el;
+    object_data.mime = "video/mp4";
+
+    if (this.wait_xhr)
+    {
+	this.log("LinternaMagica.sites.videolectures.net:\n"+
+		 "Waiting "+this.wait_xhr+
+		 " ms ("+(this.wait_xhr/1000)+
+		 " s) before requesting video link via"+
+		 " video_id "+data.video_id+" ",1);
+
+	var self = this;
+	setTimeout(function() {
+	    self.request_video_link.apply(self,[object_data]);
+	}, this.wait_xhr);
+    }
+    else
+    {
+	this.request_video_link(object_data);
+    }
+}
+
+LinternaMagica.prototype.sites["videolectures.net"].prepare_xhr = function(object_data)
+{
+    object_data.address = object_data.video_id;
+
+    return object_data;
+}
+
+
+LinternaMagica.prototype.sites["videolectures.net"].process_xhr_response =
+function(args)
+{
+    var client = args.client;
+    var object_data = args.object_data;
+
+    var xml = client.responseXML;
+
+    if (!xml)
+    {
+	return null;
+    }
+
+    var videos = xml.getElementsByTagName("video");
+
+    if (!videos)
+    {
+	return null;
+    }
+
+    var hd_links = new Array();
+
+    for (var i=0,l=videos.length; i<l; i++)
+    {
+	var video = videos[i];
+	var link = new Object();
+	var proto = video.getAttribute('proto');
+	var ext = video.getAttribute('ext');
+	link.url = video.getAttribute('src');
+	link.mime = video.getAttribute('type');
+	link.video_width = video.getAttribute('width');
+
+	if (proto == 'rtmp' || proto == 'm3u8')
+	{
+	    continue;
+	}
+
+	link.label = video.getAttribute('height') +'p ' + ext + " "+ proto;
+	hd_links.push(link);
+    }
+
+    // Sort the array by video width, so the links show properly in
+    // the HD links list with the highest quality at the top.
+    var sort_fun = function (a, b)
+    {
+	if (parseInt(a.video_width) > parseInt(b.video_width))
+	{
+	    return -1;
+	}
+	else if (parseInt(a.video_width) < parseInt(b.video_width))
+	{
+	    return 1;
+	}
+	else
+	{
+	    return 0;
+	}
+    }
+
+    hd_links.sort(sort_fun);
+
+
+    object_data.hd_links = hd_links;
+
+    object_data.link = hd_links ? hd_links[hd_links.length-1].url : null;
+
+    if (!object_data.link)
+    {
+	return null;
+    }
+
+    return object_data;
+}
+
+LinternaMagica.prototype.sites["videolectures.net"].css_fixess = function(object_data)
+{
+
+    this.pause_site_html5_player(object_data.parent);
+
+    var video = object_data.parent.getElementsByTagName('video');
+
+    if (video && video[0])
+    {
+	video[0].parentNode.removeChild(video[0]);
+    }
+
+    var controls = document.querySelectorAll('.controls');
+
+    if (controls && controls)
+    {
+	var parent = controls[0].parentNode;
+	parent.removeChild(controls[0]);
+    }
 }
